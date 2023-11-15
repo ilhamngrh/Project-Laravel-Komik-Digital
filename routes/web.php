041a10b8bff40868579b2212863d14557a890f69
ilhamngrh <nugraha.ilham100@gmail.com>
2023-11-16 06:38:35 +0700

    templating blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', [
+        'title' => "home"
+    ]);
+});
+Route::get('/posts', function () {
+    return view('posts', [
+        'title' => "posts"
+    ]);
+});
+Route::get('/about', function () {
+    return view('about', [
+        'title' => "about"
+    ]);
 });
